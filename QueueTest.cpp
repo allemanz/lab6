7cda3a3c83725b0116d5a1b3e402e350526d647f
Zach Alleman <z729a780@ku.edu>
2019-11-21 15:02:32 -0600

    Added additional test cases.

--- QueueTest.cpp
+++ QueueTest.cpp
@@ -29,6 +29,61 @@ void QueueTest::run()
   {
     std::cout << "FAILURE\n";
   }
+  q->dequeue();
+  std::cout << "Test 3: Dequeuing the number makes it empty again: ";
+  if(q->isEmpty())
+  {
+    std::cout << "SUCCESS\n";
+    score++;
+  }
+  else
+  {
+    std::cout << "FAILURE\n";
+  }
   delete q;
-
+  q = new Queue;
+  q->enqueue(1);
+  std::cout << "Test 4: Enqueuing 1 puts 1 at front of queue: ";
+  if(q->peekFront() == 1)
+  {
+    std::cout << "SUCCESS\n";
+    score++;
+  }
+  else
+  {
+    std::cout << "FAILURE\n";
+  }
+  q->enqueue(2);
+  std::cout << "Test 5: Enqueuing 2 still leaves 1 at the front of queue: ";
+  if(q->peekFront() == 1)
+  {
+    std::cout << "SUCCESS\n";
+    score++;
+  }
+  else
+  {
+    std::cout << "FAILURE\n";
+  }
+  q->dequeue();
+  std::cout << "Test 6: Dequeuing leaves 2 at front of queue: ";
+  if(q->peekFront() == 2)
+  {
+    std::cout << "SUCCESS\n";
+    score++;
+  }
+  else
+  {
+    std::cout << "FAILURE\n";
+  }
+  q->dequeue();
+  std::cout << "Test 7: Dequeuing again empties the queue: ";
+  if(q->isEmpty())
+  {
+    std::cout << "SUCCESS\n";
+    score++;
+  }
+  else
+  {
+    std::cout << "FAILURE\n";
+  }
 }
